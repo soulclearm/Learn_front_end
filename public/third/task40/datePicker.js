3b66dcc76b07b445b41e5b14281234c994b52b0d
hao m <soulclearm@sina.com>
2016-04-26 22:05:02 +0800

    日期选择完成

--- public/third/task40/datePicker.js
+++ public/third/task40/datePicker.js
@@ -2,7 +2,7 @@ var DatePicker = function(container) {
     this.container = container;
     this.date = new Date();
     this.mainEle = null;
-    this.dateEles = [];
+    this.selectedEle = null;
 
     this.init();
 }
@@ -17,19 +17,21 @@ DatePicker.prototype = {
             .css('width', '210px')
             .css('height', '240px')
             .css('border', '2px solid lightgray')
+            .css('font-family', '微软雅黑')
             .appendTo(this.container);
 
         var self = this;
 
         // 标题
         var p = $('<p>')
-            .html(this.date.getFullYear() + '年' + (this.date.getMonth() + 1) + '月')
             .css('text-align', 'center')
             .css('margin', '0')
             .css('padding', '5px')
             .css('background-color', 'rgb(200,27,1)')
             .css('color', 'white')
             .appendTo(this.mainEle);
+
+        var title = $('<strong>').addClass('title').appendTo(p);
 
         var arrLeft = $('<strong>')
             .html('<-')
@@ -60,40 +62,44 @@ DatePicker.prototype = {
 
         // 固定不变的星期
         for (var i = 0; i < 7; i++) {
-            createEle().html(this.days[i]).appendTo(this.mainEle);
+            var el = createEle().html(this.days[i]).appendTo(this.mainEle);
+            if (i === 0 || i === 6) {
+                el.css('color', 'rgb(200,17,1)');
+            }
         }
 
-        // 找到第一个日期
-        var dat = new Date(this.date);
-        dat.setDate(dat.getDate() - this.date.getDate() + 1);
-        dat.setDate(dat.getDate() - dat.getDay());
-
+        // 日期部分
         for (var i = 0; i < 42; i++) {
             var ele = createEle()
-                .html(dat.getDate())
                 .appendTo(this.mainEle);
-            if (dat.getMonth() !== this.date.getMonth()) {
-                ele.css('color', 'lightgray');
-            }
-            if (dat.getTime() === this.date.getTime()) {
-                ele.css('background-color', 'rgb(200,27,1)').css('color', 'white');
-            }
-            dat.setDate(dat.getDate() + 1);
         }
 
+        this.renderByDate(this.date);
 
+        // 点选日期事件
+        var self = this;
+        this.mainEle.click(function(e) {
+            if (e.target.nodeName === 'SPAN') {
+                var allSpan = $('span'),
+                    index = allSpan.index($(e.target)),
+                    selectedIndex = allSpan.index(self.selectedEle);
+                var dat = new Date(self.date);
+                dat.setDate(dat.getDate() + index - selectedIndex);
+                self.selectDate(dat);
+            }
+        })
     },
 
     nextMonth: function() {
-
+        var dat = new Date(this.date);
+        dat.setMonth(dat.getMonth() + 1);
+        this.selectDate(dat);
     },
 
     preMonth: function() {
-
-    },
-
-    selectEle: function() {
-
+        var dat = new Date(this.date);
+        dat.setMonth(dat.getMonth() - 1);
+        this.selectDate(dat);
     },
 
     getSelectedDate: function() {
@@ -103,5 +109,52 @@ DatePicker.prototype = {
         return y + '年' + (m < 10 ? '0' + m : m) + '月' + (d < 10 ? '0' + d : d) + '日';
     },
 
-    selectDate: function(date) {}
+    selectDate: function(date) {
+        this.selectedEle.css('background-color', '').css('color', '');
+        if (date.getMonth() === this.date.getMonth()) {
+            var allSpan = $('span'),
+                oIndex = allSpan.index(this.selectedEle);
+            var temp = allSpan.get(oIndex + date.getDate() - this.date.getDate());
+            this.selectedEle = $(temp).css('background-color', 'rgb(200,27,1)').css('color', 'white');
+        } else {
+            this.renderByDate(date);
+        }
+
+        this.date = date;
+    },
+
+    renderByDate: function(date) {
+        $('.title').html(date.getFullYear() + '年' + (date.getMonth() + 1) + '月');
+
+        // 找到第一个日期
+        var dat = new Date(date);
+        dat.setDate(dat.getDate() - date.getDate() + 1);
+        dat.setDate(dat.getDate() - dat.getDay());
+
+        var allSpan = $('span');
+        for (var i = 0; i < 42; i++) {
+            // 获取显示日子的jq对象
+            var ele = $(allSpan.get(i + 7)).html(dat.getDate());
+
+            // 不是同月的色彩变淡
+            if (dat.getMonth() !== date.getMonth()) {
+                ele.css('color', 'lightgray');
+            } else {
+                // 周六日字变红
+                if (dat.getDay() === 0 || dat.getDay() === 6) {
+                    ele.css('color', 'rgb(200,27,1)');
+                } else {
+                    ele.css('color', '');
+                }
+            }
+
+            // 被选中的日期背景变红
+            if (dat.getTime() === date.getTime()) {
+                ele.css('background-color', 'rgb(200,27,1)').css('color', 'white');
+                this.selectedEle = ele;
+            }
+
+            dat.setDate(dat.getDate() + 1);
+        }
+    }
 };
