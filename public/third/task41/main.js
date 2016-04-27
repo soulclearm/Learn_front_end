b5e982596ddd41e33dcba4a1226d4e206d5e48b8
hao m <soulclearm@sina.com>
2016-04-27 11:37:27 +0800

    任务41完成

--- public/third/task41/main.js
+++ public/third/task41/main.js
@@ -1,1 +1,6 @@
-new DatePicker($('#container'));
+new DatePicker($('#container')).select(function() {
+    console.log(this)
+    alert('选择了日期' + this.getSelectedDate());
+});
+
+$(document.body).append($('<p>随便打几个字</p>'))
